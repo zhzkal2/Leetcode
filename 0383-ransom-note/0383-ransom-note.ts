a28ea0cf4e1c68f162c333fc6a3e65503c669d79
zhzkal2 <64739786+zhzkal2@users.noreply.github.com>
2025-01-17 18:37:14 +0900

    Time: 10 ms (86.72%), Space: 52.5 MB (96.58%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -0,0 +1,19 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+
+    const magazineMap = new Map<string, number>();
+
+    for (const char of magazine){
+        magazineMap.set(char, (magazineMap.get(char)||0)+1);
+    }
+
+    for(const char of ransomNote){
+        const count = magazineMap.get(char);
+        if (!count || count ===0){
+            return false
+        }
+        magazineMap.set(char, count-1)
+    }
+
+    return true
+    
+};
