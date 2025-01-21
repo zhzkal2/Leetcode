6e789d25932bf7a0f988138cf221bdd4f0c2b374
zhzkal2 <64739786+zhzkal2@users.noreply.github.com>
2025-01-21 09:34:21 +0900

    Time: 128 ms (5.36%), Space: 56.6 MB (8.41%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.ts
@@ -0,0 +1,25 @@
+type F = (x: number) => number;
+
+function compose(functions: F[]): F {
+    if (functions.length === 0){
+        return function(x){
+            return x
+        }
+    }else{
+        return function(x){
+            let result : number =x 
+            for (let i = functions.length -1; i>=0; i--){
+                result = functions[i](result)
+                console.log(result)
+            }
+            return result;
+        }
+    }
+    
+
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
